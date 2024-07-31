4bd8d2d9a5a5c90275e7a6e390e8df081551311b
Ivan Kulik <ivankulik1978@gmail.com>
2024-07-31 22:01:14 +0300

    create declarative base model

---
+++ src/core/models/base.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(self) -> str:
+        return f"{self.__class__.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
+
+
