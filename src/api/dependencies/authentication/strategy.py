b5001d51894abde51b98f02f7c2e1b2e9ebd18ac
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-01 23:46:48 +0300

    create get jwt strategy dependency

---
+++ src/api/dependencies/authentication/strategy.py
@@ -0,0 +1,11 @@
+from fastapi_users.authentication import JWTStrategy
+
+from core.config import settings
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(
+        secret=settings.access_token.secret,
+        lifetime_seconds=settings.access_token.lifetime_seconds,
+        algorithm=settings.access_token.algorithm,
+    )
