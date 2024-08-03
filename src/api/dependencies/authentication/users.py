3708ad64d230952eb091303d787684b9f9c404aa
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-03 14:46:01 +0300

    create get_users_db dependency

---
+++ src/api/dependencies/authentication/users.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import (
+    db_helper,
+    User,
+)
+
+
+async def get_users_db(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    yield User.get_db(session=session)
