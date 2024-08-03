0b81154eb2098e3c880ca0494d13284f77447c0c
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-03 18:07:12 +0300

    add register router

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,16 +1,27 @@
 from fastapi import APIRouter
 
-from core.config import settings
-from .routers_helper import routers_helper
 from api.dependencies.authentication.backend import auth_backend
-
+from core.config import settings
+from core.schemas.user import UserRead, UserCreate
+from .routers_helper import routers_helper
 
 router = APIRouter(
     prefix=settings.api.auth,
     tags=["Auth"],
 )
 
+# /login and /logout
 router.include_router(
-    router=routers_helper.get_auth_router(auth_backend),
+    router=routers_helper.get_auth_router(
+        auth_backend,
+    ),
     prefix="/jwt",
+)
+
+# /register
+router.include_router(
+    router=routers_helper.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
 )
