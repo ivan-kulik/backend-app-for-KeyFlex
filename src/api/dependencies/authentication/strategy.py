167b8d5a225f846793110c4d6f4310a11e0580ec
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-03 22:58:07 +0300

    remove algorithm from strategy

--- src/api/dependencies/authentication/strategy.py
+++ src/api/dependencies/authentication/strategy.py
@@ -7,5 +7,4 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=settings.access_token.secret_key,
         lifetime_seconds=settings.access_token.lifetime_seconds,
-        algorithm=settings.access_token.algorithm,
     )
