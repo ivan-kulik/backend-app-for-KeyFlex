6470e49738a69e912abb1b156480904fa33d005b
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-08 14:52:11 +0300

    update function get_by_username

--- src/core/repositories/users.py
+++ src/core/repositories/users.py
@@ -1,9 +1,10 @@
-from sqlalchemy import select, ScalarResult
+from sqlalchemy import select
+
 from core.models import db_helper, User
 
 
-async def get_by_username(username: str) -> ScalarResult:
+async def get_by_username(username: str):
     async with db_helper.session_factory() as session:
         stmt = select(User).where(User.username == username)
-        user: ScalarResult = await session.scalar(stmt)
+        user = await session.scalar(stmt)
     return user
