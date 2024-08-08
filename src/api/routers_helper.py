38a50c5bfaa14edceb6efac783bba98885053dd9
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-08 15:30:35 +0300

    redefine get routers methods in FastAPIUsers

--- src/api/routers_helper.py
+++ src/api/routers_helper.py
@@ -1,12 +1,40 @@
-from fastapi_users import FastAPIUsers
+from typing import Type
+
+from fastapi import APIRouter
+from fastapi_users import FastAPIUsers, schemas
+from fastapi_users.authentication import AuthenticationBackend
 
 from core.models import User
+from core.authentication.routers import (
+    get_register_router,
+    get_auth_router,
+)
 from core.types.user_id import UserIdType
 from api.dependencies.authentication.user_manager import get_user_manager
 from api.dependencies.authentication.backend import auth_backend
 
 
-routers_helper = FastAPIUsers[User, UserIdType](
+class CustomFastAPIUsers(FastAPIUsers[User, UserIdType]):
+    def get_register_router(
+        self, user_schema: Type[schemas.U], user_create_schema: Type[schemas.UC]
+    ) -> APIRouter:
+        return get_register_router(
+            self.get_user_manager, user_schema, user_create_schema
+        )
+
+    def get_auth_router(
+        self, backend: AuthenticationBackend, requires_verification: bool = False
+    ) -> APIRouter:
+
+        return get_auth_router(
+            backend,
+            self.get_user_manager,
+            self.authenticator,
+            requires_verification,
+        )
+
+
+routers_helper = CustomFastAPIUsers(
     get_user_manager,
     [auth_backend],
 )
