29b688b3b0b1ba087e7017a403efc25253ae153f
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-11 22:48:31 +0300

    create profile table

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,8 +2,11 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "OAuthAccount",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .profile import Profile
+from .oauth_account import OAuthAccount
---
+++ src/core/models/profile.py
@@ -0,0 +1,26 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .user import User
+
+
+class Profile(Base):
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(User.id),
+    )
+    touch_typing: Mapped[str] = mapped_column(
+        String,
+    )
+    keyboard_type: Mapped[str] = mapped_column(
+        String,
+    )
+    profession: Mapped[str] = mapped_column(
+        String,
+    )
+    location: Mapped[str] = mapped_column(
+        String,
+    )
+    about_user: Mapped[str] = mapped_column(
+        String,
+    )
