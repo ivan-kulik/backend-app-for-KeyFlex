11ee47bf184ca5cfc0e5fcfd0fddcf87d1ca87a0
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-11 22:19:56 +0300

    use UserIdType in base model

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -2,19 +2,16 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 from core.config import settings
+from core.types.user_id import UserIdType
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    metadata = MetaData(
-        naming_convention=settings.db.naming_convention
-    )
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     def __tablename__(self) -> str:
         return f"{self.__name__.lower()}s"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-
-
+    id: Mapped[UserIdType] = mapped_column(primary_key=True, autoincrement=True)
