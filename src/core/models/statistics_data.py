7d25d2ca2c05c5b50477c39f27103d78cc06d26b
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-12 22:23:24 +0300

    add relationships to tables with stats

--- src/core/models/statistics_data.py
+++ src/core/models/statistics_data.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, ARRAY, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from mixins import RelationshipMixin
 from .base import Base
 from .user import User
 
@@ -14,6 +15,24 @@ class StatisticsData(Base):
     )
 
     user: Mapped["User"] = relationship(
+        back_populates="statistics_data",
+    )
+    standard_mode: Mapped["StandardModeStats"] = relationship(
+        back_populates="statistics_data",
+    )
+    extended_mode: Mapped["ExtendedModeStats"] = relationship(
+        back_populates="statistics_data",
+    )
+    text_mode: Mapped["TextModeStats"] = relationship(
+        back_populates="statistics_data",
+    )
+    english_mode: Mapped["EnglishModeStats"] = relationship(
+        back_populates="statistics_data",
+    )
+    extreme_mode: Mapped["ExtremeModeStats"] = relationship(
+        back_populates="statistics_data",
+    )
+    user_mode: Mapped["UserModeStats"] = relationship(
         back_populates="statistics_data",
     )
 
@@ -37,25 +56,61 @@ class BaseStats:
     )
 
 
-class StandardModeStats(Base, BaseStats):
+class StandardModeStats(
+    Base,
+    BaseStats,
+    RelationshipMixin[StatisticsData],
+):
     __tablename__ = "standard_mode_stats"
 
+    __relation_name = "standard_mode"
 
-class ExtendedModeStats(Base, BaseStats):
+
+class ExtendedModeStats(
+    Base,
+    BaseStats,
+    RelationshipMixin[StatisticsData],
+):
     __tablename__ = "extended_mode_stats"
 
+    __relation_name = "extended_mode"
 
-class TextModeStats(Base, BaseStats):
+
+class TextModeStats(
+    Base,
+    BaseStats,
+    RelationshipMixin[StatisticsData],
+):
     __tablename__ = "text_mode_stats"
 
+    __relation_name = "text_mode"
 
-class EnglishModeStats(Base, BaseStats):
+
+class EnglishModeStats(
+    Base,
+    BaseStats,
+    RelationshipMixin[StatisticsData],
+):
     __tablename__ = "english_mode_stats"
 
+    __relation_name = "english_mode"
 
-class ExtremeModeStats(Base, BaseStats):
+
+class ExtremeModeStats(
+    Base,
+    BaseStats,
+    RelationshipMixin[StatisticsData],
+):
     __tablename__ = "extreme_mode_stats"
 
+    __relation_name = "extreme_mode"
 
-class UserModeStats(Base, BaseStats):
+
+class UserModeStats(
+    Base,
+    BaseStats,
+    RelationshipMixin[StatisticsData],
+):
     __tablename__ = "user_mode_stats"
+
+    __relation_name = "user_mode"
