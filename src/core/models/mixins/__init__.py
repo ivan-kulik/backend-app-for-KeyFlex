26096e0adc6c5b094eda6053da4ca73cf9f85283
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-12 22:22:54 +0300

    create relationship mixin

---
+++ src/core/models/mixins/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("RelationshipMixin",)
+
+from .relationship_mixin import RelationshipMixin
---
+++ src/core/models/mixins/relationship_mixin.py
@@ -0,0 +1,15 @@
+from typing import Generic, ClassVar, TypeVar
+from sqlalchemy.orm import declared_attr, relationship, Mapped
+
+
+RelationClassName = TypeVar("RelationClassName")
+
+
+class RelationshipMixin(Generic[RelationClassName]):
+    __relation_name: ClassVar[str]
+
+    @declared_attr
+    def statistics_data(cls) -> Mapped[RelationClassName]:
+        return relationship(
+            back_populates=cls.__relation_name,
+        )
