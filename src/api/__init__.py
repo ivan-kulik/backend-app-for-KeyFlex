145a5f03c812c0205932444baa13f76b263154f0
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-17 20:41:17 +0300

    add stats router to api router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,6 +4,7 @@ from fastapi.security import HTTPBearer
 from core.config import settings
 from .auth import router as auth_router
 from .users import router as users_router
+from .stats import router as stats_router
 
 http_bearer = HTTPBearer(auto_error=False)
 
@@ -14,3 +15,4 @@ router = APIRouter(
 
 router.include_router(auth_router)
 router.include_router(users_router)
+router.include_router(stats_router)
