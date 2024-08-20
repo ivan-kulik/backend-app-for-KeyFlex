ebc92bee2916ae28384e47600f19bd69a36e55b5
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-20 13:35:36 +0300

    remove register date from UserRead pydantic schema

--- src/core/schemas/user.py
+++ src/core/schemas/user.py
@@ -1,14 +1,13 @@
 from fastapi_users import schemas
 from typing import Annotated, Optional
 from annotated_types import MinLen, MaxLen
-import datetime
+
 
 from core.types.user_id import UserIdType
 
 
 class UserRead(schemas.BaseUser[UserIdType]):
     username: str
-    registration_date: datetime.datetime
 
 
 class UserCreate(schemas.BaseUserCreate):
