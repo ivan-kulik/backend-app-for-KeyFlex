68d580e3eddb71cb6cf2d3b50bd34635372bec64
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-25 15:20:30 +0300

    add index to achievements table

--- src/core/models/achievements.py
+++ src/core/models/achievements.py
@@ -14,6 +14,7 @@ class Achievements(Base):
             ondelete="CASCADE",
         ),
         unique=True,
+        index=True,
     )
 
     forty_characters_per_minute: Mapped[bool] = mapped_column(
