452e54530d439432156dd31814faf1be88797f43
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-26 23:14:26 +0300

    create GetProfileData pydantic model

--- src/core/schemas/profile.py
+++ src/core/schemas/profile.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 
 from typing import Optional
+from datetime import date
+
+from .achievements import Achievements
 
 
 class ProfileUpdate(BaseModel):
@@ -9,3 +12,14 @@ class ProfileUpdate(BaseModel):
     profession: Optional[str] = None
     location: Optional[str] = None
     about_user: Optional[str] = None
+
+
+class GetProfileData(BaseModel):
+    name: str
+    touch_typing: bool
+    keyboard_type: str
+    profession: str
+    location: str
+    register_date: date
+    about_user: str
+    achievements: Achievements
