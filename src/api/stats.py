3fdd0f385bd7e97db67a51fb72df1a882c35d685
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-26 23:31:24 +0300

    create get_last_sessions_stats endpoint

--- src/api/stats.py
+++ src/api/stats.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, status, Depends
-
 from typing import Annotated
 
-from core.config import settings
-from core.schemas.stats import AddStatisticsData
-from core.services.stats import ModesStatsService
-from core.models import User
-from .routers_helper import routers_helper
+from fastapi import APIRouter, status, Depends
 
 from api.dependencies.stats_service import get_modes_stats_service
+from core.config import settings
+from core.models import User
+from core.schemas.stats import (
+    AddStatisticsData,
+    GetModesStatsData,
+    GetLastSessionsStatsData,
+)
+from core.services.stats import ModesStatsService
+from .routers_helper import routers_helper
 
 
 router = APIRouter(
@@ -37,15 +40,37 @@ async def add_statistics(
     )
 
 
-@router.get("/get_all_stats", status_code=status.HTTP_200_OK)
-async def get_all_statistics(
+@router.get(
+    "/get_modes_stats_data",
+    status_code=status.HTTP_200_OK,
+    response_model=GetModesStatsData,
+)
+async def get_modes_stats_data(
     user: Annotated[User, Depends(current_active_verified_user)],
     modes_stats_service: Annotated[
         ModesStatsService,
         Depends(get_modes_stats_service),
     ],
 ):
-    stats_data = await modes_stats_service.get_all_stats(
+    modes_stats_data = await modes_stats_service.get_modes_stats_data(
         cur_user=user,
     )
-    return stats_data
+    return modes_stats_data
+
+
+@router.get(
+    "/get_last_sessions_stats",
+    status_code=status.HTTP_200_OK,
+    response_model=GetLastSessionsStatsData,
+)
+async def get_last_sessions_stats(
+    user: Annotated[User, Depends(current_active_verified_user)],
+    modes_stats_service: Annotated[
+        ModesStatsService,
+        Depends(get_modes_stats_service),
+    ],
+):
+    last_sessions_stats_data = await modes_stats_service.get_last_sessions_stats_data(
+        cur_user=user,
+    )
+    return last_sessions_stats_data
