f0a91a4396cebced10263cd155c9c078d2759b48
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-29 15:38:58 +0300

    update achievements repository

--- src/core/repositories/achievements.py
+++ src/core/repositories/achievements.py
@@ -1,18 +1,37 @@
-from sqlalchemy import select
+from sqlalchemy import select, insert
 
 from core.db.db_helper import db_helper
-from .base_repository import SQLAlchemyRepository
 from core.models import Achievements
 
 
-class AchievementsRepository(SQLAlchemyRepository):
+class AchievementsRepo:
     model = Achievements
 
-    async def get_achievements(self, profile_id):
+    async def create_achievements_row(self, initial_data):
         async with db_helper.session_factory() as session:
-            stmt = select(self.model).where(self.model.profile_id == profile_id)
+            stmt = insert(self.model).values(**initial_data)
+            await session.execute(stmt)
+            await session.commit()
+
+    async def get_achievements(self, profile_id: int):
+        async with db_helper.session_factory() as session:
+            stmt = select(self.model).where(
+                self.model.profile_id == profile_id,
+            )
             data = await session.scalar(stmt)
         return data
 
-    async def change_achievements_status(self):
-        pass
+    async def change_achievements_status(
+        self,
+        profile_id: int,
+        achievements_to_update_status: list[str],
+    ):
+        async with db_helper.session_factory() as session:
+            stmt = select(self.model).where(
+                self.model.profile_id == profile_id,
+            )
+            achievements_data = await session.scalar(stmt)
+
+            for achievement in achievements_to_update_status:
+                setattr(achievements_data, achievement, True)
+            await session.commit()
