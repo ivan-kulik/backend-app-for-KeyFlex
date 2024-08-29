571b0dd21db1037544238c2f8284d55e87261890
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-29 15:39:08 +0300

    update achievements service

--- src/core/services/achievements.py
+++ src/core/services/achievements.py
@@ -1,21 +1,48 @@
-from core.repositories.achievements import AchievementsRepository
+from core.repositories import (
+    ProfileRepo,
+    AchievementsRepo,
+)
+from core.models import AchievementsEnum
 
 
 class AchievementsService:
-    def __init__(self, achievements_repo: AchievementsRepository):
-        self.achievements_repo: AchievementsRepository = achievements_repo()
+    def __init__(self, repo: AchievementsRepo):
+        self.repo: AchievementsRepo = repo()
 
     async def create_achievements_row(self, profile_id):
         data = {
             "profile_id": profile_id,
         }
-        return await self.achievements_repo.add_one(data)
+        await self.repo.create_achievements_row(
+            initial_data=data,
+        )
 
-    async def change_achievement_status(self, stats_data):
-        pass
+    async def get_achievements_to_update_status(self, symbols_per_minute: int):
+        achievements_to_update_status = []
+        for achieve in AchievementsEnum:
+            if symbols_per_minute <= achieve.value:
+                break
+            achievements_to_update_status.append(achieve.name)
+        return achievements_to_update_status
+
+    async def change_achievement_status(
+        self,
+        user_reference: str,
+        symbols_per_minute: int,
+    ):
+        achievements_to_update_status = await self.get_achievements_to_update_status(
+            symbols_per_minute=symbols_per_minute,
+        )
+        profile_id = await ProfileRepo().get_profile_id(
+            user_reference=user_reference,
+        )
+        await self.repo.change_achievements_status(
+            profile_id=profile_id,
+            achievements_to_update_status=achievements_to_update_status,
+        )
 
     async def get_achievements(self, profile_id: int):
-        data = await self.achievements_repo.get_achievements(profile_id=profile_id)
+        data = await self.repo.get_achievements(profile_id=profile_id)
 
         achievements = {
             title: val for title, val in data.__dict__.items() if type(val) is bool
