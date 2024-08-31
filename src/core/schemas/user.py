497207dd40bee21afa331b1914d583f95b835235
Ivan Kulik <ivankulik1978@gmail.com>
2024-08-31 19:51:33 +0300

    update UserUpdate pydantic schema

--- src/core/schemas/user.py
+++ src/core/schemas/user.py
@@ -1,6 +1,7 @@
 from fastapi_users import schemas
 from typing import Annotated, Optional
 from annotated_types import MinLen, MaxLen
+from pydantic import EmailStr
 
 
 from core.types.user_id import UserIdType
@@ -14,5 +15,7 @@ class UserCreate(schemas.BaseUserCreate):
     username: Annotated[str, MinLen(3), MaxLen(20)]
 
 
-class UserUpdate(schemas.BaseUserUpdate):
+class UserUpdate:
     username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
