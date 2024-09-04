24f6e4ab3d7e8e86973c6ff46c70699803ec36fd
Ivan Kulik <ivankulik1978@gmail.com>
2024-09-04 22:24:43 +0300

    change user's endpoints

--- src/api/users.py
+++ src/api/users.py
@@ -1,18 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
 
 from core.config import settings
-from .routers_helper import routers_helper
-from core.schemas.user import UserRead, UserUpdate
+from core.models import User
+
+# from .routers_helper import routers_helper
+from core.services import UserService
+from core.schemas.user import UserUpdate
+from api.dependencies import current_active_user, get_user_service
 
 router = APIRouter(
     prefix=settings.api.users,
     tags=["Users"],
 )
 
-# /me and /{id}
-router.include_router(
-    router=routers_helper.get_users_router(
-        UserRead,
-        UserUpdate,
-    ),
-)
+# # /me and /{id}
+# router.include_router(
+#     router=routers_helper.get_users_router(
+#         UserRead,
+#         UserUpdate,
+#     ),
+# )
+
+
+# api/users/me - PATCH
+# api/users/me - DELETE
+
+
+@router.patch("/me")
+async def update_user_data(
+    cur_user: Annotated[User, Depends(current_active_user)],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+    user_update: UserUpdate,
+) -> None:
+    await user_service.update_user_data(
+        cur_user=cur_user,
+        user_update_data=user_update,
+    )
+
+
+@router.delete("/me")
+async def delete_account(
+    cur_user: Annotated[User, Depends(current_active_user)],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+) -> None:
+    await user_service.delete_account(cur_user=cur_user)
