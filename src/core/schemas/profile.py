8ddaf962a1094ce9fb756f32c33ffed062c999b9
Ivan Kulik <ivankulik1978@gmail.com>
2024-09-20 00:19:14 +0300

    delete profile update pydantic model

--- src/core/schemas/profile.py
+++ src/core/schemas/profile.py
@@ -6,14 +6,6 @@ from datetime import date
 from .achievements import Achievements
 
 
-class ProfileUpdate(BaseModel):
-    touch_typing: Optional[bool] = None
-    keyboard_type: Optional[str] = None
-    profession: Optional[str] = None
-    location: Optional[str] = None
-    about_user: Optional[str] = None
-
-
 class GetProfileData(BaseModel):
     name: str
     touch_typing: bool
